3dc24eddc7c3ab2883271b55bed438acbbdc1a47
Rodrigo Benjamin Sanchez Fuentes <60365489+FEMTO-I@users.noreply.github.com>
2020-02-21 12:52:51 -0800

    21/02/20

---
+++ Pelicula/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pelicula
+{
+    class Peliculas
+    {
+        public string Titulo;
+        public Int16  Año;
+        public string Pais;
+        public string Director;
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peliculas p1=new Peliculas();
+            Peliculas p2=new Peliculas();
+             p1.SetTitulo("La La Land");
+             p1.SetAño(2016); 
+             Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+
+
+            
+        }
+    }
+}
